2734ba5a1fb95a8d9c6eaed7b33ff1664f20ab7c
TechnologyK <773091523@qq.com>
2023-06-08 14:54:14 +0800

    隐藏lastUpdated和contributors

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,10 @@
-import { defineUserConfig } from "vuepress";
+import { defineUserConfig, defaultTheme } from "vuepress";
 
 export default defineUserConfig({
   lang: "zh-CN",
-  title: "TechnologyK个人博客"
+  title: "TechnologyK个人博客",
+  theme: defaultTheme({
+    lastUpdated: false,
+    contributors: false,
+  }),
 });
